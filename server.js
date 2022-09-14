fce806ff066b6ff37d82cbb0a602303f6094d746
Duy <trantheduyk13@gmail.com>
2022-09-14 17:26:00 +0700

    update code and travis

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
+  while (true) {
     console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
